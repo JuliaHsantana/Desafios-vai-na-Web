86420784caeec1b03aaa1e828db953a74dcc0d1e
JuliaHsantana <contato.juliahsantana@gmail.com>
2023-12-08 22:56:39 -0300

    primeiro completo

---
+++ Script.js
@@ -0,0 +1,29 @@
+console.log("Ano novo chegando!");
+let cantora ="Mariah Carey";
+let idade=54;
+let ano=2023;
+
+
+
+const anoNascimento = ano - idade;
+
+console.log(anoNascimento);
+let quartaFeira=true;
+console.log(quartaFeira);
+console.log(typeof quartaFeira);
+
+let suaIdade=21;
+
+if(suaIdade>=18){
+    console.log("Você é maior de idade, poderá curtir o show do Roberto Carlos com participação especial de Mariah Carey")
+}else{
+    console.log("Sinto muito, mas assistirá da tv globo")
+};
+
+let niver = "Agosto";
+
+if (niver == "Agosto" || niver == "Dezembro" || niver == "Junho") {
+    console.log(`Uma ou mais opções estão corretas. O mês digitado foi ${niver}.`);
+} else {
+    console.log(`Algo de errado não está certo. O mês digitado foi ${niver}.`);
+}
